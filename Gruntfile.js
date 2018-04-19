5fed46e2cc37457f06582d582b6d7419e690144c
Claudio Sanches <contato@claudiosanches.com>
2018-04-19 12:14:08 -0300

    Initial files

---
+++ Gruntfile.js
@@ -0,0 +1,271 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig({
+
+		// Setting folder templates.
+		dirs: {
+			css: 'assets/css',
+			fonts: 'assets/fonts',
+			images: 'assets/images',
+			js: 'assets/js'
+		},
+
+		// JavaScript linting with JSHint.
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/admin/*.js',
+				'!<%= dirs.js %>/admin/*.min.js',
+				'<%= dirs.js %>/frontend/*.js',
+				'!<%= dirs.js %>/frontend/*.min.js'
+			]
+		},
+
+		// Sass linting with Stylelint.
+		stylelint: {
+			options: {
+				configFile: '.stylelintrc'
+			},
+			all: [
+				'<%= dirs.css %>/*.scss'
+			]
+		},
+
+		// Minify .js files.
+		uglify: {
+			options: {
+				ie8: true,
+				parse: {
+					strict: false
+				},
+				output: {
+					comments : /@license|@preserve|^!/
+				}
+			},
+			admin: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/admin/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.js %>/admin/',
+					ext: '.min.js'
+				}]
+			},
+			frontend: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/frontend/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.js %>/frontend/',
+					ext: '.min.js'
+				}]
+			}
+		},
+
+		// Compile all .scss files.
+		sass: {
+			compile: {
+				options: {
+					sourceMap: 'none'
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.css %>/',
+					src: ['*.scss'],
+					dest: '<%= dirs.css %>/',
+					ext: '.css'
+				}]
+			}
+		},
+
+		// Generate RTL .css files
+		rtlcss: {
+			woocommerce: {
+				expand: true,
+				cwd: '<%= dirs.css %>',
+				src: [
+					'*.css',
+					'!select2.css',
+					'!*-rtl.css'
+				],
+				dest: '<%= dirs.css %>/',
+				ext: '-rtl.css'
+			}
+		},
+
+		// Minify all .css files.
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: '<%= dirs.css %>/',
+				src: ['*.css'],
+				dest: '<%= dirs.css %>/',
+				ext: '.css'
+			}
+		},
+
+		// Watch changes for assets.
+		watch: {
+			css: {
+				files: ['<%= dirs.css %>/*.scss'],
+				tasks: ['sass', 'rtlcss', 'cssmin']
+			},
+			js: {
+				files: [
+					'<%= dirs.js %>/admin/*js',
+					'<%= dirs.js %>/frontend/*js',
+					'!<%= dirs.js %>/admin/*.min.js',
+					'!<%= dirs.js %>/frontend/*.min.js'
+				],
+				tasks: ['jshint', 'uglify']
+			}
+		},
+
+		// Generate POT files.
+		makepot: {
+			options: {
+				type: 'wp-plugin',
+				domainPath: 'i18n/languages',
+				potHeaders: {
+					'report-msgid-bugs-to': 'https://github.com/woocommerce/woocommerce-product-type-column/issues',
+					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+				}
+			},
+			dist: {
+				options: {
+					potFilename: 'woocommerce-product-type-column.pot',
+					exclude: [
+						'vendor/.*',
+						'tests/.*',
+						'tmp/.*'
+					]
+				}
+			}
+		},
+
+		// Check textdomain errors.
+		checktextdomain: {
+			options:{
+				text_domain: 'woocommerce-product-type-column',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php',               // Include all files
+					'!node_modules/**',       // Exclude node_modules/
+					'!tests/**',              // Exclude tests/
+					'!vendor/**',             // Exclude vendor/
+					'!tmp/**'                 // Exclude tmp/
+				],
+				expand: true
+			}
+		},
+
+		// Autoprefixer.
+		postcss: {
+			options: {
+				processors: [
+					require( 'autoprefixer' )({
+						browsers: [
+							'> 0.1%',
+							'ie 8',
+							'ie 9'
+						]
+					})
+				]
+			},
+			dist: {
+				src: [
+					'<%= dirs.css %>/*.css'
+				]
+			}
+		}
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-rtlcss' );
+	grunt.loadNpmTasks( 'grunt-postcss' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'js',
+		'css',
+		'i18n'
+	]);
+
+	grunt.registerTask( 'js', [
+		'jshint',
+		'uglify:admin',
+		'uglify:frontend'
+	]);
+
+	grunt.registerTask( 'css', [
+		'sass',
+		'rtlcss',
+		'postcss',
+		'cssmin',
+		'concat'
+	]);
+
+	grunt.registerTask( 'docs', [
+		'clean:apidocs',
+		'shell:apidocs'
+	]);
+
+	// Only an alias to 'default' task.
+	grunt.registerTask( 'dev', [
+		'default'
+	]);
+
+	grunt.registerTask( 'i18n', [
+		'checktextdomain',
+		'makepot'
+	]);
+
+	grunt.registerTask( 'e2e-tests', [
+		'shell:e2e_tests'
+	]);
+
+	grunt.registerTask( 'e2e-tests-grep', [
+		'shell:e2e_tests_grep'
+	]);
+
+	grunt.registerTask( 'e2e-test', [
+		'shell:e2e_test'
+	]);
+};
---
+++ woocommerce-product-type-column.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Product Type Column
+ * Plugin URI:  https://github.com/woocommerce/woocommerce-product-type-column
+ * Description: @todo
+ * Author:      WooCommerce
+ * Author URI:  https://woocommerce.com
+ * Version:     1.0.0
+ * License:     GPLv3
+ * Text Domain: woocommerce-product-type-column
+ * Domain Path: /languages
+ *
+ * Copyright (C) 2017 WooCommerce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package WooCommerce_Product_Type_Column
+ */
+
+defined( 'ABSPATH' ) || exit;
